4c8480f009c4808c7dea92c3f3672428e6d7de98
SanaGo108 <oksana135@mail.ru>
2024-07-26 23:53:26 +0300

    add new

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from datetime import datetime
+import pytz
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    timezone = pytz.timezone('Europe/Moscow')  # Установка часового пояса
+    now = datetime.now(timezone)
+    current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    return render_template('home.html', current_time=current_time)  # Передача переменной в шаблон
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
